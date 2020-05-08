0c2548e0e28b5fbbca63484eb3e951e9f7b408cf
ms-jasondel <jasondel@microsoft.com>
2020-05-08 18:15:20 -0400

    Found workaroun for AllowAnyOrigin and AllowCredentials.

--- AspNetCoreSignalR_React.Server/Startup.cs
+++ AspNetCoreSignalR_React.Server/Startup.cs
@@ -12,15 +12,15 @@ namespace AspNetCoreSignalR_React.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors(o => o.AddPolicy("CorsPolicy", builder =>
-            {
-                builder
-                    .AllowAnyMethod()
-                    .AllowAnyHeader()
-                    .AllowCredentials()
-                    .WithOrigins("http://localhost:3000");
-            }));
-
+            services.AddCors(o => o.AddDefaultPolicy(builder =>
+                {
+                    builder
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .SetIsOriginAllowed(_ => true)
+                        .AllowCredentials();
+                })
+            );
             services.AddSignalR();
             services.AddHostedService<MyBackgroundService>();
         }
@@ -28,8 +28,8 @@ namespace AspNetCoreSignalR_React.Server
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors();
             app.UseRouting();
-            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(routes =>
             {
